3b2779ccfb40b97adfc6cc482a5b0a02ca1a9d02
davidtweddell <david.tweddell@gmail.com>
2024-01-28 20:02:03 -0500

    updates

--- project_modules/io/__init__.py
+++ project_modules/io/__init__.py
@@ -1,7 +1,9 @@
 # TODO: add frequently used io functions
 
 from ._save_tgz import save_tgz
+from ._load_datasets import load_dataset_to_df
 
 all = [
         "save_tgz",
+        "load_dataset_to_df"
     ]
