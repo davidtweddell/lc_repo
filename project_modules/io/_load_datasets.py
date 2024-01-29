7958b2e2cc161c025154b77f0e28575d12c3f173
davidtweddell <david.tweddell@gmail.com>
2024-01-28 20:01:59 -0500

    updates

---
+++ project_modules/io/_load_datasets.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from datasets.arrow_dataset import Dataset
+from datasets.dataset_dict import DatasetDict
+import logging
+
+
+#==============================================================================
+def load_dataset_to_df(input_path: str,
+                       verbose: bool = False) -> pd.DataFrame:
+#==============================================================================
+# load a dataset dict and convert it to a dataframe
+    
+    from project_modules.data_prep import join_datasets
+
+    # load the dataset dict
+    if verbose:
+        logging.info(f"Loading dataset dict from {input_path}...")
+    dsd = DatasetDict.load_from_disk(input_path)
+
+    # convert to a combined dataframe
+    if verbose:
+        logging.info("Converting to dataframe...")
+    df = join_datasets(dsd)
+
+    if verbose:
+        logging.info(f"Shape: {df.shape}")
+
+    return df
+#==============================================================================
+
+
+
