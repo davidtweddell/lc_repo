4ae5a0f13a81dbe9d610ee9b75c581a6456d3ef3
ubugpu <mtrx303.dev@gmail.com>
2024-04-14 17:25:59 -0400

    Classifiers 81 83 CA+Bourta+Validation, init classify module w classifyMP and boruta fs

---
+++ project_modules/classifcation/__init__.py
@@ -0,0 +1,7 @@
+# TODO: add any common clustering tools
+
+from ._classify import classify_MP
+from ._classify import getXY
+from ._classify import boruta_fs
+
+all = ['getXY',"classify_MP",boruta_fs]
