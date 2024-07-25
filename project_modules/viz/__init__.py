d1bc028c0c485f3635ec8335cd5d5c008034b8ee
davidtweddell <david.tweddell@gmail.com>
2024-07-25 10:51:58 -0400

    added excursion plot

--- project_modules/viz/__init__.py
+++ project_modules/viz/__init__.py
@@ -4,20 +4,42 @@
 # - [x] generic scatterplot
 # - [x] generic barplot
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-import pandas as pd
-import colorcet as cc
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import numpy as np
+# import pandas as pd
+# import colorcet as cc
 
 from ._plot_bar import plot_bar
 from ._plot_heatmap import plot_heatmap
 from ._plot_histogram import plot_histogram
 from ._plot_scatter import plot_scatter
+from ._excursion_plot import excursion_plot
 
-all = [ 
-        "plot_bar",
-        "plot_heatmap", 
-        "plot_histogram"
-        "plot_scatter",
-       ]
+# # heatmap parameters
+# heatmap_parms = {
+#                 'cbar': False,
+#                 'square': False,
+#                 'linewidths': 0.5,
+#                 'linecolor': 'black',
+#                 'center': 0.0,
+#                 'vmin': -1,
+#                 'vmax': 1,
+#                 # 'annot': False,
+#                 'fmt': '.2f',
+# }
+
+__all__ = [ 
+                        "plot_bar",
+                        "plot_heatmap", 
+                        "plot_histogram"
+                        "plot_scatter",
+                        "excursion_plot"
+                ]
+# all = [ 
+#         "plot_bar",
+#         "plot_heatmap", 
+#         "plot_histogram"
+#         "plot_scatter",
+#         "excursion_plot"
+#        ]
